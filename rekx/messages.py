0601fda9dfc6c67e55bc9b50986359dca83ab735
Nikos Alexandris <nikosalexandris@duck.com>
2023-12-14 02:09:23 +0200

    Add messages.py

---
+++ rekx/messages.py
@@ -0,0 +1,5 @@
+from rekx.hardcodings import x_mark
+
+
+NOT_IMPLEMENTED_CLI = '[bold red]Yet not implemented![/bold red]'
+ERROR_IN_SELECTING_DATA = f'[red blink]{x_mark}[/red blink] [red on white]Something went wrong in [bold]selecting[/bold] the data[/red on white]'
