4840ee6b3b0ed5c4b84cef9fb55d0caa6f2bb767
Nikos Alexandris <nikosalexandris@duck.com>
2023-12-18 09:49:38 +0200

    [Major] Massively update diagnose.py
    Add :
    - get_netcdf_metadata()
    - get_multiple_netcdf_metadata()
    - collect_netcdf_metadata()

--- rekx/diagnose.py
+++ rekx/diagnose.py
@@ -1,26 +1,153 @@
 from loguru import logger
-import typer
-from rekx.typer_parameters import OrderCommands
-from typing_extensions import Annotated
-from pathlib import Path
-from rekx.typer_parameters import typer_argument_source_directory
-from rekx.typer_parameters import typer_option_filename_pattern
-from typing import List
-from .models import XarrayVariableSet
+import os
+from netCDF4 import Dataset
+import pandas as pd
+import numpy as np
 import xarray as xr
-from .models import select_xarray_variable_set_from_dataset
+from typing_extensions import Annotated
+from typing import List
+from pathlib import Path
 from concurrent.futures import ProcessPoolExecutor
 from concurrent.futures import as_completed
-from rekx.typer_parameters import typer_option_csv
-from rekx.typer_parameters import typer_option_verbose
-from rekx.constants import VERBOSE_LEVEL_DEFAULT
-from rekx.progress import DisplayMode
-from rekx.progress import display_context
+from rich import print
+import typer
+from .typer_parameters import OrderCommands
+from .typer_parameters import typer_argument_source_directory
+from .typer_parameters import typer_option_filename_pattern
+from .models import XarrayVariableSet
+from .models import select_xarray_variable_set_from_dataset
+from .models import select_netcdf_variable_set_from_dataset
+from .typer_parameters import typer_option_csv
+from .typer_parameters import typer_option_verbose
+from .constants import VERBOSE_LEVEL_DEFAULT
+from .progress import DisplayMode
+from .progress import display_context
 from .print import print_chunk_shapes_table
 from .csv import write_nested_dictionary_to_csv
 from .rich_help_panel_names import rich_help_panel_diagnose
 from .print import print_common_chunk_layouts
-from rich import print
+
+
+def format_compression(compression_dict):
+    if isinstance(compression_dict, dict):
+        # Keep only keys with True values
+        return ', '.join([key for key, value in compression_dict.items() if value])
+    return compression_dict
+
+
+def get_netcdf_metadata(
+    input_netcdf_path: Path,
+    variable: str = None,
+    variable_set: Annotated[XarrayVariableSet, typer.Option(help="Set of Xarray variables to diagnose")] = XarrayVariableSet.all,
+    csv: Annotated[Path, typer_option_csv] = None,
+    verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
+):
+    """
+    """
+    if not os.path.exists(input_netcdf_path):
+        return "File not found: " + input_netcdf_path
+
+    with Dataset(input_netcdf_path, 'r') as dataset:
+        metadata = {
+            "File name": input_netcdf_path.name,
+            "File size": os.path.getsize(input_netcdf_path),  # in Bytes
+            "Dimensions": {
+                dim: len(dataset.dimensions[dim]) for dim in dataset.dimensions
+            },
+        }
+        variable_metadata = {}
+        selected_variables = select_netcdf_variable_set_from_dataset(
+            XarrayVariableSet, variable_set, dataset
+        )
+        for variable_name in selected_variables:
+            variable = dataset[variable_name]  # variable is not a simple string anymore!
+            variable_metadata[variable_name] = {
+                'Shape': ' x '.join(map(str, variable.shape)),
+                'Chunks': ' x '.join(map(str, variable.chunking())),
+                'Cache*': ' x '.join(map(str, variable.get_var_chunk_cache())),
+                'Type': str(variable.dtype),
+                'Scale': getattr(variable, 'scale_factor', '-'),
+                'Offset': getattr(variable, 'add_offset', '-'),
+                'Compression': variable.filters() if 'filters' in dir(variable) else '-',
+                'Shuffling': getattr(variable, 'shuffle', '-')
+            }
+        metadata['Variables'] = variable_metadata
+
+    if verbose:
+        from .print import print_metadata_table
+        print_metadata_table(metadata)
+
+    return metadata, input_netcdf_path
+
+
+def get_multiple_netcdf_metadata(
+    file_paths: List[Path],
+    variable: str = None,
+    variable_set: XarrayVariableSet = XarrayVariableSet.all,
+    csv: Path = None,
+    verbose: int = VERBOSE_LEVEL_DEFAULT,
+):
+    """
+    """
+    metadata_series = {}
+    with ProcessPoolExecutor() as executor:
+        futures = [
+            executor.submit(
+                get_netcdf_metadata,
+                file_path,
+                variable,
+                variable_set.value,
+            )
+            for file_path in file_paths
+        ]
+        for future in as_completed(futures):
+            try:
+                metadata, input_netcdf_path = future.result()
+                # logger.info(f"Scanned file: {input_netcdf_path}")
+                metadata_series[input_netcdf_path.name] = metadata
+            except Exception as e:
+                logger.error(f"Error processing file: {e}")
+
+    return metadata_series
+
+
+from typing import Optional
+def collect_netcdf_metadata(
+    source_directory: Annotated[Path, typer_argument_source_directory],
+    pattern: Annotated[str, typer_option_filename_pattern] = "*.nc",
+    variable_set: Annotated[XarrayVariableSet, typer.Option(help="Set of Xarray variables to diagnose")] = XarrayVariableSet.all,
+    group_metadata: Annotated[Optional[bool], 'Visually cluster rows of metadata per input NetCDF file and variable'] = False,
+    csv: Annotated[Path, typer_option_csv] = None,
+    verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
+):
+    """Scan files in the source directory that match the pattern and diagnose the chunking shapes for each variable."""
+    source_directory = Path(source_directory)
+    file_paths = list(source_directory.glob(pattern))
+    mode = DisplayMode(verbose)
+    with display_context[mode]:
+        try:
+            metadata_series = get_multiple_netcdf_metadata(
+                    file_paths=file_paths,
+                    variable_set=variable_set,
+            )
+            # print(f'Metadata series : {metadata_series}')
+        except TypeError as e:
+            raise ValueError("Error occurred:", e)
+
+    # from .print import print_metadata_table
+    # for metadata in metadata_series:
+        # print_metadata_table(metadata_series)
+    from .print import print_metadata_series_table
+    print_metadata_series_table(
+            metadata_series=metadata_series,
+            group_metadata=group_metadata,
+    )
+
+    if csv:
+        write_nested_dictionary_to_csv(
+            nested_dictionary=metadata_series,
+            output_filename=csv,
+        )
 
 
 def detect_chunking_shapes(
@@ -114,7 +241,6 @@ def diagnose_chunking_shapes(
     verbose: Annotated[int, typer_option_verbose] = VERBOSE_LEVEL_DEFAULT,
 ):
     """Scan files in the source directory that match the pattern and diagnose the chunking shapes for each variable."""
-
     source_directory = Path(source_directory)
     file_paths = list(source_directory.glob(pattern))
     mode = DisplayMode(verbose)
