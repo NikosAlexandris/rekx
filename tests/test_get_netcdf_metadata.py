1dcbcb09ec1c40a4bbf76d32a4a3d5a3ad210592
Nikos Alexandris <nikosalexandris@duck.com>
2024-01-15 02:18:35 +0200

    [RevisitMe] Update test_get_netcdf_metadata.py
    Add some to-be-tests !

--- tests/test_get_netcdf_metadata.py
+++ tests/test_get_netcdf_metadata.py
@@ -2,11 +2,33 @@ from pathlib import Path
 
 import pytest
 
-from rekx.diagnose import get_netcdf_metadata
+from rekx.netcdf_metadata import get_netcdf_metadata
 
 
 def test_file_not_found():
     """Test if FileNotFoundError is raised for a non-existent file"""
-    non_existent_file = Path("/path/to/non/existent/file.nc")
     with pytest.raises(FileNotFoundError):
-        get_netcdf_metadata(non_existent_file)
+        get_netcdf_metadata("/path/to/non/existent/file.nc")
+
+
+def test_metadata():
+    input_file = Path("test_data.nc")
+    metadata, path = get_netcdf_metadata(input_file)
+    assert isinstance(metadata, dict)
+    assert "File name" in metadata
+    assert "File size" in metadata
+    assert "Dimensions" in metadata
+    assert "Repetitions" in metadata
+    assert isinstance(path, Path)
+    assert path == input_file
+
+
+def test_specific_variable_metadata(variable):
+    input_file = Path("test_data.nc")
+    variable_name = variable
+    metadata, path = get_netcdf_metadata(input_file, variable=variable_name)
+    assert variable_name in metadata["Variables"]
+    variable_metadata = metadata["Variables"][variable_name]
+    assert "Shape" in variable_metadata
+    assert "Chunks" in variable_metadata
+    assert "Cache" in variable_metadata
